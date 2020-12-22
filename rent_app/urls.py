c88ac1724242df5951949fb46f60998ab9165356
samerimr <imraish.09@gmail.com>
2020-12-22 00:17:49 -0800

    init commit

---
+++ rent_app/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from . import views	# the . indicates that the views file can be found in the same directory as this file
+                    
+urlpatterns = [
+    path('', views.index),
+]
